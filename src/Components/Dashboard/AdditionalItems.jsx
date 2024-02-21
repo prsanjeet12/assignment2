64ad5c6fc842728c9b4117d591e321a3e2bc2f80
ppanwar <ppanwar@cholachariots.com>
2024-02-21 14:01:58 +0530

    new updates

---
+++ src/Components/Dashboard/AdditionalItems.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import group11 from '../../assets/Group 11.png';
+import group12 from '../../assets/Group 12.png';
+import group10 from '../../assets/Group 10.png';
+import group13 from '../../assets/Group 13.png';
+
+const AdditionalItems = () => {
+  return (
+    <div className="mt-4 ml-[30px] text-[14px] flex flex-wrap gap-9 text-[#8D969E] font-medium">
+      <div className="flex-1 mb-4">
+        <img src={group11} alt="" />
+        <p className=''>Rendering Jobs</p>
+        <span className="font-bold text-black text-[24px]">10</span>
+      </div>
+      <div className="flex-1 mb-4">
+        <img src={group12} alt="" />
+        <p>Waiting Jobs</p>
+        <span className="font-bold text-black text-[24px]">15</span>
+      </div>
+      <div className="flex-1 mb-4">
+        <img src={group10} alt="" />
+        <p>Completed Jobs</p>
+        <span className="font-bold text-black text-[24px]">20</span>
+      </div>
+      <div className="flex-1 mb-4">
+        <img src={group13} alt="" />
+        <p>Failed Jobs</p>
+        <span className="font-bold text-black text-[24px]">25</span>
+      </div>
+    </div>
+  );
+};
+
+export default AdditionalItems;
--- src/Components/Dashboard/Sidebar.jsx
+++ src/Components/Dashboard/Sidebar.jsx
@@ -40,7 +40,7 @@ const Sidebar = () => {
 };
 
 const MenuItem = ({ icon, text, active = false }) => {
-  const menuItemClass = active ? "bg-blue-500 text-white pb-1 rounded-md text-center flex tex-center px-5" : "text-[#8D969E] hover:text-[#FFD166]";
+  const menuItemClass = active ? "bg-blue-500 text-white pb-6 h-12 rounded-md text-center flex tex-center px-5" : "text-[#8D969E] hover:text-[#FFD166]";
   
   return (
     <div className={`flex items-center cursor-pointer mb-2 ${menuItemClass}`}>
